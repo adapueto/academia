875f87dad3d8fcda4efacb1f8b2ff35aa6e33f10
Juan Pablo <j.pabloblanco@gmail.com>
2020-10-28 20:48:33 -0300

    Update pianoame.ino

--- pianoame.ino
+++ pianoame.ino
@@ -24,36 +24,28 @@ void setup() {
 
 
 void loop() { // Comienzo del loop
-
   //Programación Piano
-  if ( digitalRead(5) == LOW) {
+  for(int i=5;i<9;i++){
+    if(digitalRead(i) == HIGH){
+      noTone(9);
+    }
+  }
+  
+  if (digitalRead(5) == LOW) {
     tone(9, 523.25);
     delay(1000);
   }
-  else {
-    noTone(9);
-  }
+  
   if ( digitalRead(6) == LOW) {
     tone(9, 659.26);
     delay(1000);
-  }
-  else {
-    noTone(9);
   }
   if ( digitalRead(7) == LOW) {
     tone(9, 783.99);
     delay(1000);
   }
-  else {
-    noTone(9);
-  }
   if ( digitalRead(8) == LOW) {
     tone(9, 880);
     delay(1000);
   }
-  else {
-    noTone(9);
-  }
-
-
 } 
